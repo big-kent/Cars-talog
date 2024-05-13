cf6d4015223863c2ddc82ebc5d5f6d50bc6182d2
Scorpse <lyminhphucsupreme1010@gmail.com>
2024-05-13 09:03:28 +0700

    Name of the models will change according to the URL

--- src/components/DetailPage/DotsStepper.js
+++ src/components/DetailPage/DotsStepper.js
@@ -5,6 +5,7 @@ import IconButton from '@mui/material/IconButton';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Typography from '@mui/material/Typography';
+import { useParams } from 'react-router-dom';
 
 
 
@@ -27,11 +28,12 @@ const DotsStepper = () => {
                         <ChevronRightIcon className='mr-12 bg-white rounded-full' fontSize='large'/>
                     </IconButton>
     }
+    const { model } = useParams();
 
     return (
         <>
             <div className='relative'> 
-                <Typography theme={theme} variant='h1' style={{ position: 'absolute', zIndex: '10', color:'white', bottom: '10rem', right: '5rem', align: 'center', marginTop: '2rem', fontFamily: 'Arvo', fontStyle: 'serif', textTransform: 'uppercase'}}>車 name</Typography>
+                <Typography theme={theme} variant='h1' style={{ position: 'absolute', zIndex: '10', color:'white', bottom: '10rem', right: '5rem', align: 'center', marginTop: '2rem', fontFamily: 'Arvo', fontStyle: 'serif', textTransform: 'uppercase'}}>{model}</Typography>
                 
                     <Slide {...buttonProperties} indicators={index => <div className ="indicator"></div>} scale={1} duration='2000'>
                         <div className ="each-slide-effect">
